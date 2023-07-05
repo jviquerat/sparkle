a59554b34df7e8797b1b94f2752c2d822e75a1c4
jviquerat <jonathan.viquerat@mines-paristech.fr>
2023-07-05 14:03:35 +0200

    W

---
+++ sparkle/src/utils/timer.py
@@ -0,0 +1,33 @@
+# Generic imports
+import time
+
+# Custom imports
+from sparkle.src.utils.prints import *
+
+###############################################
+### timer class
+### Used to measure time spent on different operations
+class timer():
+    def __init__(self, name):
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.time_tic = None
+        self.time_toc = None
+        self.dt       = 0.0
+
+    def tic(self):
+        self.t_tic = time.perf_counter()
+
+    def toc(self, show=False):
+        self.t_toc = time.perf_counter()
+        self.dt   += self.t_toc - self.t_tic
+
+        if (show): self.show()
+
+    def show(self):
+        str_dt = str(f"{self.dt:.2f}")
+        spacer()
+        print("Timer "+str(self.name)+": "+str_dt+" seconds")
+        self.reset()
