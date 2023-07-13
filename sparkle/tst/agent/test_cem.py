ef5fb82889f13badf84b3ee8eeb277cc6381376c
jviquerat <jonathan.viquerat@mines-paristech.fr>
2023-07-13 15:13:45 +0200

    W

---
+++ sparkle/tst/agent/test_cem.py
@@ -0,0 +1,17 @@
+# Generic imports
+import pytest
+
+# Custom imports
+from sparkle.tst.tst    import *
+from sparkle.tst.runner import *
+
+###############################################
+### Test pso
+def test_pso():
+
+    # Add environment to PATH
+    sys.path.append("sparkle/env/parabola")
+
+    # Run test
+    runner("sparkle/tst/agent/parabola_cem.json",
+           "pso", 1.0e-5, 1.0e-7)
