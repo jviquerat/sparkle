9182e63580a7053fd3d7dccf08052965a6097356
jviquerat <jonathan.viquerat@mines-paristech.fr>
2023-07-24 10:54:13 +0200

    W

---
+++ sparkle/src/env/multiproc_environments.py
@@ -0,0 +1,132 @@
+# Generic imports
+import os
+import sys
+import numpy           as np
+import multiprocessing as mp
+
+# Custom imports
+from sparkle.src.env.multiproc_worker import *
+from sparkle.src.utils.timer          import *
+
+###############################################
+### A wrapper class for multiprocessing parallel environments
+class multiproc_environments:
+    def __init__(self, path, pms):
+
+        # Default parameters
+        self.name  = pms.name
+        self.args  = None
+        self.pipes = []
+        self.procs = []
+
+        if hasattr(pms, "args"): self.args = pms.args
+
+        # Start environments
+        for env in range(parallel.size()):
+            p_pipe, c_pipe = mp.Pipe()
+            process        = mp.Process(target = multiproc_worker,
+                                        args   = (self.name, self.args,
+                                                  env, path, c_pipe))
+
+            self.pipes.append(p_pipe)
+            self.procs.append(process)
+
+            process.daemon = True
+            process.start()
+
+        # Initialize timer
+        self.timer_env = timer("env      ")
+
+    # Return dimension of environment
+    def dim(self):
+
+        # Send
+        self.pipes[0].send(('dim', None))
+
+        # Receive
+        d = self.pipes[0].recv()
+
+        return d
+
+    # Return xmin value
+    def xmin(self):
+
+        # Send
+        self.pipes[0].send(('xmin', None))
+
+        # Receive
+        xm = self.pipes[0].recv()
+
+        return xm
+
+    # Return xmax value
+    def xmax(self):
+
+        # Send
+        self.pipes[0].send(('xmax', None))
+
+        # Receive
+        xm = self.pipes[0].recv()
+
+        return xm
+
+    # Compute cost in all environments
+    def cost(self, x):
+
+        # Initialize stuff
+        n_dof   = x.shape[0]
+        costs   = np.zeros((n_dof))
+        n_loops = n_dof//parallel.size()
+
+        self.timer_env.tic()
+
+        for i in range(n_loops):
+
+            # Send
+            for p in range(parallel.size()):
+                p.send(('step', x[i*parallel.size()+p]))
+
+            # Receive
+            for p in range(parallel.size()):
+                c = self.pipes[p].recv()
+                costs[i*parallel.size()+p] = c
+
+        self.timer_env.toc()
+
+        return costs
+
+    # Reset environments
+    def reset(self, run):
+
+        # Send
+        for p in self.pipes:
+            p.send(('reset', None))
+
+        # Receive
+        data = np.array([])
+        for p in self.pipes:
+            r    = p.recv()
+            data = np.append(data, r)
+
+        return data
+
+    # Render environment
+    def render(self, x):
+
+        # Send
+        self.pipes[0].send(('render', x))
+
+        # Receive
+        rnd = self.pipes[0].recv()
+
+        return rnd
+
+    # Close
+    def close(self):
+
+        # Close all envs
+        for p in self.pipes:
+            p.send(('close', None))
+        for p in self.procs:
+            p.terminate()
+            p.join()
