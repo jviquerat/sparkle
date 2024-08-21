153e132f2909fbe3872f603a69d9819962085e95
Jonathan <jonathan.viquerat@mines-paristech.fr>
2024-08-21 16:04:49 +0200

    Added a transparent optimizer class accessible directly in python without building a full environment

---
+++ sparkle/src/agent/optimizer.py
@@ -0,0 +1,69 @@
+# Generic imports
+import types
+import numpy as np
+
+# Local factory
+from sparkle.src.core.factory import factory
+from sparkle.src.agent.pso    import pso
+from sparkle.src.agent.cmaes  import cmaes
+from sparkle.src.agent.cem    import cem
+from sparkle.src.agent.pbo    import pbo
+
+local_factory = factory()
+
+local_factory.register("pso",   pso)
+local_factory.register("cmaes", cmaes)
+local_factory.register("cem",   cem)
+local_factory.register("pbo",   pbo)
+
+###############################################
+### The optimizer class is a transparent class for calling
+### optimizers with a simple cost function, without the need
+### to write a full environment
+### It does not comply to the other agents interface
+### It is assumed to run sequentially
+class optimizer():
+    def __init__(self, name, dim, x0, xmin, xmax, n_points, n_steps_max, cost):
+
+        agent_pms             = types.SimpleNamespace()
+        agent_pms.n_points    = n_points
+        agent_pms.n_steps_max = n_steps_max
+        agent_pms.silent      = True
+        agent_pms.clip        = True
+
+        # Initialize agent
+        self.agent = local_factory.create(name,
+                                          path = ".",
+                                          dim  = dim,
+                                          x0   = x0,
+                                          xmin = xmin,
+                                          xmax = xmax,
+                                          pms  = agent_pms)
+
+        # Cost function
+        self.cost = cost
+
+        # Initial reset
+        self.reset(0)
+
+    # Reset
+    def reset(self, run):
+
+        self.agent.reset(run)
+
+    # Perform optimization
+    def optimize(self):
+
+        # Loop until done
+        while (not self.agent.done()):
+
+            x = self.agent.dof()
+
+            n_pts = x.shape[0]
+            c     = np.zeros(n_pts)
+            for p in range(n_pts):
+                c[p] = self.cost(x[p])
+
+            self.agent.step(c)
+
+        return self.agent.best_x, self.agent.best_score
---
+++ sparkle/tst/agent/test_optimizer.py
@@ -0,0 +1,34 @@
+# Generic imports
+import pytest
+import numpy as np
+
+# Custom imports
+from sparkle.src.agent.optimizer import optimizer
+from sparkle.tst.tst             import *
+
+###############################################
+### Test transparent optimizer
+def test_optimizer():
+
+    # Parabola test function
+    def parabola(x):
+
+        v = 0.0
+        for i in range(len(x)):
+            v += (x[i])**2
+
+        return v
+
+    name        = "cmaes"
+    dim         = 2
+    x0          = 2.5*np.ones(dim)
+    xmin        =-5.0*np.ones(dim)
+    xmax        = 5.0*np.ones(dim)
+    n_points    = 10
+    n_steps_max = 30
+
+    opt  = optimizer(name, dim, x0, xmin, xmax, n_points, n_steps_max, parabola)
+    x, c = opt.optimize()
+
+    assert(np.all(np.abs(x) < 1.0e-3))
+    assert(c < 1.0e-6)
