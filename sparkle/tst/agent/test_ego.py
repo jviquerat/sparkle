6871b9013c338a11c3b2d0e4ecee24b6ff506f2d
Jonathan <jonathan.viquerat@mines-paristech.fr>
2024-11-13 10:10:47 +0100

    ego: added test

---
+++ sparkle/tst/agent/test_ego.py
@@ -0,0 +1,17 @@
+# Generic imports
+import pytest
+
+# Custom imports
+from sparkle.tst.tst    import *
+from sparkle.tst.runner import *
+
+###############################################
+### Test ego
+def test_ego():
+
+    # Add environment to PATH
+    sys.path.append("sparkle/env/parabola")
+
+    # Run test
+    runner("sparkle/tst/agent/parabola_ego.json",
+           "ego", 10.0, 1.0e-3)
