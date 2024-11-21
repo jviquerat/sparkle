d4bf76e0e3309ad03574f1c80c0e07c7cd5509f5
Jonathan <jonathan.viquerat@mines-paristech.fr>
2024-11-21 16:28:45 +0100

    error: added a call_exit optional argument, modified printing calls

--- sparkle/src/utils/error.py
+++ sparkle/src/utils/error.py
@@ -1,23 +1,25 @@
 # Custom imports
-from sparkle.src.utils.prints import liner, spacer, new_line
+from sparkle.src.env.parallel import parallel
+from sparkle.src.utils.prints import liner, spacer, new_line, err_print
 
 ###############################################
 ### A set of functions to print errors and warnings
 
 ### Error
-def error(module, function, text):
+def error(module, function, text, call_exit=True):
     liner()
-    errr("Sparkle error")
+    err_print("Sparkle error")
     spacer()
     print("Module "+str(module)+", function "+str(function))
     spacer()
     print(text)
-    exit(1)
 
+    if (call_exit): exit(1)
+    
 ### Warning
 def warning(module, function, text):
     liner()
-    warn("Sparkle warning")
+    warn_print("Sparkle warning")
     spacer()
     print("Module "+str(module)+", function "+str(function))
     spacer()
