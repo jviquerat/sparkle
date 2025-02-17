459134716caedb282e469489d59c78e9cf2c942a
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-17 09:36:33 +0100

    W

---
+++ sparkle/src/model/sepnet.py
@@ -0,0 +1,214 @@
+# Generic imports
+import math
+import torch
+import torch.optim as toptim
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Custom imports
+from sparkle.src.network.lip_mlp     import lip_mlp
+from sparkle.src.optimizer.optimizer import opt_factory
+from sparkle.src.env.spaces          import environment_spaces
+
+###############################################
+### Separable network model
+class sepnet():
+    def __init__(self, r_dim, spaces, pms_opt):
+
+        self.r_dim   = r_dim
+        self.spaces  = spaces
+        self.pms_opt = pms_opt
+
+        self.reset()
+
+    # Reset model
+    def reset(self):
+
+        # Initialize networks
+        self.net = []
+        for k in range(self.r_dim):
+            net = lip_mlp(inp_dim   = 1,
+                          out_dim   = 1,
+                          arch      = [32,32,32],
+                          acts      = ["tanh","tanh","tanh","linear"],
+                          lip_const = [2.0, 2.0, 2.0, 5.0])
+
+            self.net.append(net)
+
+        self.opt   = toptim.Adam([{"params": self.net[0].params()},
+                                  {"params": self.net[1].params()}], lr=5.0e-4)
+
+    # Normalize input
+    def normalize_input(self, x):
+
+        xx  = x - 0.5*(self.spaces.xmin() + self.spaces.xmax())
+        xx /= 0.5*(self.spaces.xmax() - self.spaces.xmin())
+
+        return xx
+
+    # Normalize output
+    def normalize_output(self, y):
+
+        yy  = y - 0.5*(self.ymin + self.ymax)
+        yy /= 0.5*(self.ymax - self.ymin)
+
+        return yy
+
+    # Denormalize output
+    def denormalize_output(self, yy):
+
+        y = yy*0.5*(self.ymax - self.ymin)
+        y += 0.5*(self.ymin + self.ymax)
+
+        return y
+
+    # Evaluate at test points
+    def evaluate(self, x):
+
+        xt         = torch.tensor(self.normalize_input(x))
+        batch_size = x.shape[0]
+        r          = torch.zeros(batch_size, self.r_dim)
+
+        for k in range(self.r_dim):
+            xk     = torch.reshape(xt[:,k], (-1,1))
+            r[:,k] = torch.reshape(self.net[k].forward(xk), (batch_size,1))[:,0]
+
+        return r
+
+    # Build model from input
+    def build(self, x, y):
+
+        self.reset()
+
+        n_points     = x.shape[0]
+        n_epochs     = 30000
+        batch_size   = math.floor(1.0*n_points)
+        history_loss = np.zeros((n_epochs, 2))
+        xn           = self.normalize_input(x)
+
+        self.ymax = np.max(y)
+        self.ymin = np.min(y)
+        yn = self.normalize_output(y)
+
+        for epoch in range(n_epochs):
+
+            r = np.arange(0, n_points)
+            p = np.random.permutation(r)
+
+            xb = torch.from_numpy(xn[p])
+            c  = torch.from_numpy(yn[p])
+
+            done = False
+            btc  = 0
+
+            while not done:
+                start = btc*batch_size
+                end   = min((btc+1)*batch_size, n_points)
+                btc  += 1
+                if (end == n_points): done = True
+
+                loss = self.get_loss(xb[start:end], c[start:end])
+
+                self.opt.zero_grad()
+                loss.backward()
+                self.opt.step()
+
+                history_loss[epoch,1] += loss
+
+            history_loss[epoch,0]   = epoch
+            history_loss[epoch,1] /= float(btc)
+
+            end = "\r"
+            if (epoch == n_epochs-1): end = "\n"
+            print("# Epoch #"+str(epoch)+"/"+str(n_epochs)+", loss = "+str(history_loss[epoch,1])+"                    ", end=end)
+
+        self.plot_loss("loss.png", history_loss, ["loss"])
+
+    # Compute loss
+    # x shape is [batch_size, dim]
+    # c shape is [batch_size]
+    def get_loss(self, x, c):
+
+        x.requires_grad = True
+        batch_size      = x.shape[0]
+        r               = torch.zeros(batch_size, self.r_dim)
+
+        for k in range(self.r_dim):
+            xk     = torch.reshape(x[:,k], (-1,1))
+            r[:,k] = torch.reshape(self.net[k].forward(xk), (batch_size,1))[:,0]
+
+        r_sum = torch.sum(r, axis=1)
+        loss  = torch.mean((c - r_sum)**2)
+
+        return loss
+
+    # Plot losses
+    # loss is a np.array of shape (n_samples, n_losses+1)
+    # the first column is the epoch number
+    def plot_loss(self, filename, loss, labels):
+
+        plt.clf()
+        fig = plt.figure()
+
+        for k in range(loss.shape[1]-1):
+            plt.plot(loss[:,0], loss[:,k+1], label=labels[k])
+
+        plt.yscale('log')
+        plt.legend(loc="upper right")
+        plt.savefig(filename, dpi=100)
+        plt.close()
+
+    # Plot function for 2D models only
+    def plot_model_2d(self, x_pts, f_cost):
+
+        n_plot = 100
+        x      = np.linspace(self.spaces.xmin()[0],
+                             self.spaces.xmax()[0], num=n_plot)
+        y      = np.linspace(self.spaces.xmax()[1],
+                             self.spaces.xmin()[1], num=n_plot)
+        grid   = np.array(np.meshgrid(x, y))
+        X      = grid.reshape(self.r_dim,-1).T
+        z      = np.zeros((n_plot, n_plot, 3))
+
+        r        = self.evaluate(X)
+        rr       = torch.sum(r, axis=1).detach()
+        rr       = self.denormalize_output(rr)
+        #z[:,:,0] = torch.sum(r, axis=1).detach().reshape(n_plot,n_plot)
+        z[:,:,0] = rr.reshape(n_plot,n_plot)
+        z[:,:,1] = np.reshape(f_cost(X), (n_plot,n_plot))
+        z[:,:,2] = np.abs(z[:,:,1] - z[:,:,0])
+
+        titles = [None]*(3)
+        # for k in range(self.r_dim):
+        #     titles[k] = "f_"+str(k)+"(x_"+str(k)+")"
+        titles[0] = "sum(f_k)"
+        titles[1] = "f(x)"
+        titles[2] = "f(x) - sum(f_k)"
+
+        plt.clf()
+        fig = plt.figure()
+        fig.set_size_inches(5*3+3, 5)
+        fig.subplots_adjust(0,0,1,1)
+
+        for k in range(3):
+            ax = fig.add_subplot(1,3,k+1)
+            ax.set_xlim([self.spaces.xmin()[0], self.spaces.xmax()[0]])
+            ax.set_ylim([self.spaces.xmin()[1], self.spaces.xmax()[1]])
+            ax.axis('off')
+            ax.set_title(titles[k])
+            ax.imshow(z[:,:,k],
+                      extent=[self.spaces.xmin()[0], self.spaces.xmax()[0],
+                              self.spaces.xmin()[1], self.spaces.xmax()[1]],
+                      vmin=self.spaces.vmin(), vmax=self.spaces.vmax(),
+                      alpha=0.8, cmap='RdBu_r')
+            if (k == 1):
+                ax.scatter(x_pts[:,0], x_pts[:,1], c="black", marker='o', alpha=0.8)
+            cnt = ax.contour(x, y, z[:,:,k],
+                             levels=self.spaces.levels(),
+                             colors='black', alpha=0.5)
+            ax.clabel(cnt, inline=True, fontsize=8, fmt="%.0f")
+
+        filename = "rewards.png"
+        plt.savefig(filename, dpi=100)
+        plt.close()
---
+++ sparkle/src/trainer/separable.py
@@ -0,0 +1,65 @@
+# Generic imports
+import torch
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Custom imports
+from sparkle.src.trainer.base    import base_trainer
+from sparkle.src.agent.agent     import agent_factory
+from sparkle.src.utils.timer     import timer
+from sparkle.src.env.parallel    import parallel
+from sparkle.src.utils.error     import error
+from sparkle.src.pex.pex         import pex_factory
+from sparkle.src.model.sepnet    import sepnet
+
+###############################################
+### Class for separable trainer
+class separable(base_trainer):
+    def __init__(self, env_pms, agent_pms, path, pms):
+
+        # Set parameters
+        self.r_dim = pms.r_dim
+        self.render_every = 100000
+        if hasattr(pms, "render_every"): self.render_every = pms.render_every
+
+        # Initialize environment
+        self.env = parallel.environments(path, env_pms)
+
+        # Initialize pex
+        self.pex = pex_factory.create(pms.pex.name,
+                                      spaces = self.env.spaces,
+                                      pms    = pms.pex)
+
+        # Initialize agent
+        self.agent = agent_factory.create(agent_pms.name,
+                                          path   = path,
+                                          spaces = self.env.spaces,
+                                          pms    = agent_pms)
+
+        self.model = sepnet(r_dim   = self.r_dim,
+                            spaces  = self.env.spaces,
+                            pms_opt = pms.opt)
+
+        # Initialize timer
+        self.timer_global = timer("global   ")
+
+    # Reset
+    def reset(self, run):
+
+        self.env.reset(run)
+        self.pex.reset()
+        self.model.reset()
+
+    # Optimize
+    def optimize(self):
+
+        costs = np.zeros(self.pex.n_points())
+        for i in range(self.pex.n_points()):
+            costs[i] = self.env.cost(self.pex.point(i))
+
+        self.model.build(self.pex.x(), costs)
+
+        self.model.plot_model_2d(self.pex.x(), self.env.cost)
+
+        exit()
