aef92016a6d08ee05fa7b6b73f4d75f43d76014c
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-17 09:34:49 +0100

    spaces: added plotting data

--- sparkle/src/env/spaces.py
+++ sparkle/src/env/spaces.py
@@ -6,14 +6,30 @@ import numpy as np
 class environment_spaces:
     def __init__(self, spaces, pms=None):
 
-        self.dim_  = spaces[0]
-        self.x0_   = spaces[1]
-        self.xmin_ = spaces[2]
-        self.xmax_ = spaces[3]
+        self.natural_dim_ = spaces[0]
+        self.true_dim_    = self.natural_dim_
+        self.x0_          = spaces[1]
+        self.xmin_        = spaces[2]
+        self.xmax_        = spaces[3]
+
+        if (len(spaces) > 4):
+            self.vmin_   = spaces[4]
+            self.vmax_   = spaces[5]
+            self.levels_ = spaces[6]
+
+        self.separable_ = False
+        if hasattr(pms, "separable"): self.separable = pms.separable
+
+        if (self.separable_):
+            self.true_dim_ = 1
 
     # Accessor
     def dim(self):
-        return self.dim_
+        return self.true_dim_
+
+    # Accessor
+    def natural_dim(self):
+        return self.true_dim_
 
     # Accessor
     def x0(self):
@@ -26,3 +42,15 @@ class environment_spaces:
     # Accessor
     def xmax(self):
         return self.xmax_
+
+    # Accessor
+    def vmin(self):
+        return self.vmin_
+
+    # Accessor
+    def vmax(self):
+        return self.vmax_
+
+    # Accessor
+    def levels(self):
+        return self.levels_
