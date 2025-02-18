fe04dad52a292c8b07ec1eff4fe977a10e81628a
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-18 09:40:14 +0100

    pex: added fixed poisson-disc sampling

---
+++ sparkle/src/pex/fixed_poisson_disc.py
@@ -0,0 +1,103 @@
+# Generic imports
+import math
+import numpy as np
+
+# Custom imports
+from sparkle.src.pex.base    import base_pex
+from sparkle.src.utils.error import error
+
+###############################################
+### Poisson-disc experiment plan
+### Relies on Robert Bridson algorithm
+### XXX 2D only for now
+class fixed_poisson_disc(base_pex):
+    def __init__(self, spaces, pms):
+
+        super().__init__(spaces)
+
+        self.name_       = "fixed_poisson_disc"
+        self.n_points_   = pms.n_points
+        self.n_attempts_ = pms.n_attempts
+
+        # Compute guess of radius
+        self.radius_ = 0.5*math.sqrt(self.volume()/self.n_points_)
+
+        self.reset()
+
+    # Reset sampling
+    # We start by generating a fine poisson-disk sampling, then
+    # apply a furthest point sampling on the resulting set to
+    # ensure that we have exactly n_points_
+    def reset(self):
+
+        # Poisson-disk sampling
+        p = np.random.uniform(low  = self.xmin(),
+                              high = self.xmax(),
+                              size = self.dim())
+
+        lst    = [p]
+        active = [p]
+
+        while (len(active) > 0):
+            found  = False
+            k      = np.random.randint(0, len(active))
+            theta  = np.random.uniform(low  = 0.0,
+                                       high = 2.0*math.pi,
+                                       size = self.n_attempts_)
+            radius = np.random.uniform(low  = self.radius_,
+                                       high = 2.0*self.radius_,
+                                       size = self.n_attempts_)
+
+            for i in range(self.n_attempts_):
+                x  = active[k][0] + radius[i]*math.cos(theta[i])
+                y  = active[k][1] + radius[i]*math.sin(theta[i])
+                pt = np.array([x,y])
+
+                if ((pt[0] < self.xmin()[0]) or
+                    (pt[0] > self.xmax()[0]) or
+                    (pt[1] < self.xmin()[1]) or
+                    (pt[1] > self.xmax()[1])): continue
+
+                ok = True
+                for j in range(len(lst)):
+                    dist = self.distance(pt, lst[j])
+                    if (dist < self.radius_):
+                        ok = False
+                        break
+
+                if ok:
+                    lst.append(pt)
+                    active.append(pt)
+                    found = True
+
+            if not found:
+                active.pop(k)
+
+        # Check that we have more than n_points_
+        if (len(lst) < self.n_points_):
+            error("poisson_disk", "reset",
+                  "the resulting number of points was lower than n_points_")
+
+        # Farthest point sampling
+        k = np.random.randint(0, len(lst))
+        selected = [lst[k]]
+        lst.pop(k)
+
+        while (len(selected) < self.n_points_):
+            distances = np.zeros((len(lst), len(selected)))
+            for i in range(len(lst)):
+                for j in range(len(selected)):
+                    distances[i,j] = self.distance(lst[i], selected[j])
+
+            min_dists = np.min(distances, axis=1)
+            k         = np.argmax(min_dists)
+
+            selected.append(lst[k])
+            lst.pop(k)
+
+        self.x_ = np.array(selected)
+
+    # Distance
+    def distance(self, p1, p2):
+
+        return np.linalg.norm(p1 - p2, 2)
--- sparkle/src/pex/pex.py
+++ sparkle/src/pex/pex.py
@@ -1,12 +1,14 @@
 # Custom imports
-from sparkle.src.core.factory import factory
-from sparkle.src.pex.random   import random
-from sparkle.src.pex.lhs      import lhs
+from sparkle.src.core.factory           import factory
+from sparkle.src.pex.random             import random
+from sparkle.src.pex.lhs                import lhs
+from sparkle.src.pex.fixed_poisson_disc import fixed_poisson_disc
 
 # Declare factory
 pex_factory = factory()
 
 # Register pex
-pex_factory.register("random", random)
-pex_factory.register("lhs",    lhs)
+pex_factory.register("random",             random)
+pex_factory.register("lhs",                lhs)
+pex_factory.register("fixed_poisson_disc", fixed_poisson_disc)
 
