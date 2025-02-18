df0e1ae8ac8464931bb1c1a3baf478a733f8db10
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-18 12:08:51 +0100

    torch_dicts: removed 2D layers

--- sparkle/src/network/torch_dicts.py
+++ sparkle/src/network/torch_dicts.py
@@ -39,39 +39,3 @@ def add_fc_layer(net, dim_in, dim_out, activation):
         n += 1
 
     return n
-
-# Helper function to append a layer to a ModuleList
-def add_conv2d_layer(net, in_ch, out_ch, kernel, stride, padding, activation, w, h):
-
-    net.append(tnn.Conv2d(in_ch, out_ch, kernel, stride, padding))
-    n = 1
-
-    w_out = shape(w, kernel, padding, stride)
-    h_out = shape(h, kernel, padding, stride)
-
-    if (activation != "linear"):
-        net.append(torch_activations[activation])
-        n += 1
-
-    return n, w_out, h_out
-
-# Helper function to append a layer to a ModuleList
-def add_maxpool2d_layer(net, kernel, stride, w, h):
-
-    net.append(tnn.MaxPool2d(kernel, stride))
-    n = 1
-
-    w_out = shape(w, kernel, 0, stride)
-    h_out = shape(h, kernel, 0, stride)
-
-    return n, w_out, h_out
-
-# Compute output shape for conv layers
-# w = width (and height, assuming square images)
-# k = kernel size
-# p = padding
-# s = stride
-# d = dilation
-def shape(w, k=3, p=0, s=1, d=1):
-
-    return int((w + 2*p - d*(k - 1) - 1)/s + 1)
