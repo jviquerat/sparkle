d7d157c8007610593e3595e4fb86ccd2baf4d0ef
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-19 12:05:11 +0100

    tst::network: separating test files

--- sparkle/tst/network/test_branched_mlp.py
+++ sparkle/tst/network/test_branched_mlp.py
@@ -5,51 +5,7 @@ import numpy as np
 
 # Custom imports
 from sparkle.tst.tst                  import *
-from sparkle.src.network.mlp          import mlp
 from sparkle.src.network.branched_mlp import branched_mlp
-from sparkle.src.network.lip_mlp      import lip_mlp
-
-###############################################
-### Test mlp
-def test_mlp():
-
-    # mlp with input of size 3, output of size 1
-    net = mlp(inp_dim = 3,
-              out_dim = 1,
-              arch    = [8,8,8],
-              acts    = ["relu"])
-
-    # input with size 3 and batch_size 1
-    x = torch.zeros(1,3)
-    y = net.forward(x)
-    assert(y.shape == (1,1))
-
-    # input with size 3 and batch_size 4
-    x = torch.zeros(4,3)
-    y = net.forward(x)
-    assert(y.shape == (4,1))
-
-    # mlp with input of size 3, output of size 2
-    net = mlp(inp_dim = 3,
-              out_dim = 2,
-              arch    = [8,8,8],
-              acts    = ["relu"])
-
-    # input with size 3 and batch_size 4
-    x = torch.zeros(4,3)
-    y = net.forward(x)
-    assert(y.shape == (4,2))
-
-    # mlp with specified activations for each layer
-    net = mlp(inp_dim = 3,
-              out_dim = 2,
-              arch    = [8,8,8],
-              acts    = ["relu","relu","relu"])
-
-    # input with size 3 and batch_size 4
-    x = torch.zeros(4,3)
-    y = net.forward(x)
-    assert(y.shape == (4,2))
 
 ###############################################
 ### Test branched_mlp
@@ -121,39 +77,3 @@ def test_branched_mlp():
     x = torch.zeros(4,3)
     y = net.forward(x)
     assert(y.shape == (2,4,2))
-
-###############################################
-### Test lipschitz mlp
-def test_lip_mlp():
-
-    # lip mlp with input of size 3, output of size 1, lip_constant of 1
-    net = lip_mlp(inp_dim      = 3,
-                  out_dim      = 1,
-                  arch         = [8,8,8],
-                  acts         = ["relu"],
-                  lip_constant = 1.0)
-
-    # input with size 3 and batch_size 1
-    x0 = 1.0*torch.ones(1,3)
-    x1 = 2.0*torch.ones(1,3)
-    y0 = net.forward(x0)
-    y1 = net.forward(x1)
-    dx = tensor_distance(x0, x1)
-    dy = tensor_distance(y0, y1)
-    assert(dy <= dx)
-
-    # lip mlp with input of size 3, output of size 1, lip_constant of 0.1
-    net = lip_mlp(inp_dim      = 3,
-                  out_dim      = 1,
-                  arch         = [8,8,8],
-                  acts         = ["relu"],
-                  lip_constant = 0.01)
-
-    # input with size 3 and batch_size 1
-    x0 = 1.0*torch.ones(1,3)
-    x1 = 100.0*torch.ones(1,3)
-    y0 = net.forward(x0)
-    y1 = net.forward(x1)
-    dx = tensor_distance(x0, x1)
-    dy = tensor_distance(y0, y1)
-    assert(dy <= 0.01*dx)
---
+++ sparkle/tst/network/test_mlp.py
@@ -0,0 +1,50 @@
+# Generic imports
+import pytest
+import torch
+import numpy as np
+
+# Custom imports
+from sparkle.tst.tst                  import *
+from sparkle.src.network.mlp          import mlp
+
+###############################################
+### Test mlp
+def test_mlp():
+
+    # mlp with input of size 3, output of size 1
+    net = mlp(inp_dim = 3,
+              out_dim = 1,
+              arch    = [8,8,8],
+              acts    = ["relu"])
+
+    # input with size 3 and batch_size 1
+    x = torch.zeros(1,3)
+    y = net.forward(x)
+    assert(y.shape == (1,1))
+
+    # input with size 3 and batch_size 4
+    x = torch.zeros(4,3)
+    y = net.forward(x)
+    assert(y.shape == (4,1))
+
+    # mlp with input of size 3, output of size 2
+    net = mlp(inp_dim = 3,
+              out_dim = 2,
+              arch    = [8,8,8],
+              acts    = ["relu"])
+
+    # input with size 3 and batch_size 4
+    x = torch.zeros(4,3)
+    y = net.forward(x)
+    assert(y.shape == (4,2))
+
+    # mlp with specified activations for each layer
+    net = mlp(inp_dim = 3,
+              out_dim = 2,
+              arch    = [8,8,8],
+              acts    = ["relu","relu","relu"])
+
+    # input with size 3 and batch_size 4
+    x = torch.zeros(4,3)
+    y = net.forward(x)
+    assert(y.shape == (4,2))
