f5ed6de7914cb5e3c1ed75847c13e76370eadcff
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-19 11:06:28 +0100

    set_seeds: moved to tst.py

--- sparkle/tst/tst.py
+++ sparkle/tst/tst.py
@@ -1,9 +1,16 @@
 # Generic imports
 import warnings
 import torch
+import numpy as np
 
 # Filter warning messages
 warnings.filterwarnings('ignore',category=DeprecationWarning)
+
+# Set numpy and torch seeds
+def set_seeds(seed):
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
 
 # Compare two floats with given accuracy
 def compare(x, y, eps=1.0e-8):
