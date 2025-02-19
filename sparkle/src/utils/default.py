f8269e1e634ea2d9778b6948663fba6fc94b2307
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-19 11:05:02 +0100

    utils: added a set_default function

---
+++ sparkle/src/utils/default.py
@@ -0,0 +1,5 @@
+# Helper function to set default value if user-specified value is not present
+def set_default(name, default_value, pms):
+
+    if (hasattr(pms, name)): return getattr(pms, name)
+    else: return default_value
