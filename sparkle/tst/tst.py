929bdfa788eb7fcfcf5a514e8dce6cc766326a2c
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-19 08:48:32 +0100

    tst: added float comparison

--- sparkle/tst/tst.py
+++ sparkle/tst/tst.py
@@ -3,3 +3,8 @@ import warnings
 
 # Filter warning messages
 warnings.filterwarnings('ignore',category=DeprecationWarning)
+
+# Compare two floats with given accuracy
+def compare(x, y, eps=1.0e-8):
+
+    return math.abs(x-y) < eps
