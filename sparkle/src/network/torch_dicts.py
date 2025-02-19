e5d2fd6e07937e802dafc14b5254d63a37f0f4a2
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-19 10:08:47 +0100

    network: added lipschitz mlp

--- sparkle/src/network/torch_dicts.py
+++ sparkle/src/network/torch_dicts.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as tnn
 import numpy    as np
 
+# Custom imports
+from sparkle.src.network.torch_layers import lipschitz_linear
+
 torch_activations = {
     'relu': tnn.ReLU(),
     'prelu': tnn.PReLU(),
@@ -22,18 +25,32 @@ torch_activations = {
 
 # Set numpy and torch seeds
 def set_seeds(seed):
-    
+
     torch.manual_seed(seed)
     np.random.seed(seed)
 
 # Helper function to append a layer to a ModuleList
 def add_mlp_layer(net, dim_in, dim_out, activation):
 
-    n  = 0
+    n = 0
     net.append(tnn.Linear(dim_in, dim_out))
 
     std = math.sqrt(1.0/dim_in)
     tnn.init.normal_(net[-1].weight, mean=0.0, std=std)
+
+    n += 1
+
+    if (activation != "linear"):
+        net.append(torch_activations[activation])
+        n += 1
+
+    return n
+
+# Helper function to append a layer to a ModuleList
+def add_lip_layer(net, dim_in, dim_out, activation, lip_constant):
+
+    n = 0
+    net.append(lipschitz_linear(dim_in, dim_out, lip_constant=lip_constant))
 
     n += 1
 
---
+++ sparkle/src/network/torch_layers.py
@@ -0,0 +1,22 @@
+# Generic imports
+import torch
+import torch.nn as tnn
+
+###############################################
+# A linear lipschitz layer
+class lipschitz_linear(tnn.Module):
+    def __init__(self, dim_in, dim_out, lip_constant=1.0):
+        super().__init__()
+
+        # Lipschitz constant
+        self.lip_constant = lip_constant
+
+        # Normalize weights to 1
+        self.linear = tnn.Linear(dim_in, dim_out)
+        self.linear = tnn.utils.spectral_norm(self.linear)
+
+    # We scale the normalized weights by the lipschitz constant
+    def forward(self, x):
+
+        scaled_weight = self.lip_constant*self.linear.weight
+        return tnn.functional.linear(x, scaled_weight, self.linear.bias)
