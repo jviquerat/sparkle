cfc6791247d8a17e3b385f97e5f34f1a7753b4bb
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-02-27 21:30:18 +0100

    agent: added lbfgsb and multi-start lbfgsb

---
+++ sparkle/src/agent/ms_lbfgsb.py
@@ -0,0 +1,40 @@
+# Generic imports
+import types
+import numpy as np
+
+# Custom imports
+from sparkle.src.agent.lbfgsb    import lbfgsb
+from sparkle.src.pex.maximin_lhs import maximin_lhs
+from sparkle.src.env.spaces      import environment_spaces
+from sparkle.src.utils.prints    import spacer
+
+###############################################
+### Multi-start L-BFGS-B
+class ms_lbfgsb():
+
+    def __init__(self):
+        pass
+
+    def optimize(self, f, xmin, xmax, n_pts=10, m=5, tol=1e-3, max_iter=20):
+
+        pms          = types.SimpleNamespace()
+        pms.n_points = n_pts
+        pms.n_iter   = 1000
+
+        dim        = xmin.shape[0]
+        space_dict = {"dim": dim, "x0": None, "xmin": xmin, "xmax": xmax}
+        spaces     = environment_spaces(space_dict)
+        pex        = maximin_lhs(spaces, pms)
+
+        x_star = np.zeros((n_pts, dim))
+        c_star = np.zeros(n_pts)
+        opt    = lbfgsb()
+
+        for k in range(n_pts):
+            x, c      = opt.optimize(f, pex.x[k], xmin, xmax, m, tol, max_iter)
+            x_star[k] = x
+            c_star[k] = c
+
+        best = np.argmin(c_star)
+
+        return x_star[best], c_star[best]
