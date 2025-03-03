3af9967114ff3483c251c9781d8650b302cbdab5
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-03 14:51:50 +0100

    infill: added log_ei

--- sparkle/src/infill/infill.py
+++ sparkle/src/infill/infill.py
@@ -1,10 +1,12 @@
 # Custom imports
 from sparkle.src.core.factory  import factory
 from sparkle.src.infill.ei     import ei
+from sparkle.src.infill.log_ei import log_ei
 
 # Declare factory
 infill_factory = factory()
 
 # Register kernels
-infill_factory.register("ei", ei)
+infill_factory.register("ei",     ei)
+infill_factory.register("log_ei", log_ei)
 
