576522a975ad5892f6deb37f07a2f050f0d84a7a
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-04 11:34:45 +0100

    kernel: parameters bounds are now based on sample min and max distances

--- sparkle/src/kernel/base.py
+++ sparkle/src/kernel/base.py
@@ -14,6 +14,15 @@ class base_kernel():
     def __init__(self, spaces):
 
         self.spaces = spaces
+        self.reset()
+
+    def reset(self):
+
+        self.x0_       = None
+        self.xmin_     = None
+        self.xmax_     = None
+        self.theta_    = None
+        self.diag_eps_ = 1.0e-15
 
     @property
     def theta(self):
@@ -21,11 +30,18 @@ class base_kernel():
 
     def optimize(self, x, y):
 
+        # Copy (x,y)
         self.x_  = x
         self.y_  = y
         self.ns_ = x.shape[0] # nb of samples
         self.nf_ = x.shape[1] # nb of features
 
+        # Update bounds
+        dmin, dmax = self.distances(self.x_)
+        self.xmin_ = np.log(np.array([max(dmin,0.1)]))
+        self.xmax_ = np.log(np.array([dmax]))
+
+        # Optimize
         opt  = ms_lbfgsb()
         x, c = opt.optimize(self.log_likelihood,
                             self.xmin_,
@@ -66,3 +82,16 @@ class base_kernel():
                 if (i==j): K[i,j] += self.diag_eps_
 
         return K
+
+    # Compute dmin and dmax on sample set
+    def distances(self, x):
+
+        dmin = 1.0e8
+        dmax = 0.0
+        for k in range(x.shape[0]):
+            for l in range(k+1, x.shape[0]):
+                d = np.linalg.norm(x[k] - x[l])
+                if (d < dmin): dmin = d
+                if (d > dmax): dmax = d
+
+        return dmin, dmax
--- sparkle/src/kernel/gaussian.py
+++ sparkle/src/kernel/gaussian.py
@@ -11,17 +11,7 @@ class gaussian(base_kernel):
     def __init__(self, spaces, pms):
         super().__init__(spaces)
 
-        self.dim_      = 1
-        self.x0_       = np.zeros(self.dim_)
-        self.xmin_     =-2.0*np.ones(self.dim_)
-        self.xmax_     = np.zeros(self.dim_)
-        self.diag_eps_ = 1.0e-15
-
-        self.reset()
-
-    def reset(self):
-
-        self.theta_ = np.exp(0.5*(self.xmin_ + self.xmax_))
+        self.dim_ = 1
 
     # Compute isotropic covariance function
     def covariance(self, xi, xj, theta):
