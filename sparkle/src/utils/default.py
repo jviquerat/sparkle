ff604edf4d1ab23162302e75b6bcba95a3f08484
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-13 16:29:09 +0100

    utils::default: cosmetics

--- sparkle/src/utils/default.py
+++ sparkle/src/utils/default.py
@@ -1,4 +1,6 @@
-# Helper function to set default value if user-specified value is not present
+###############################################
+### Helper function to set default value if
+### user-specified value is not present
 def set_default(name, default_value, pms):
 
     if (hasattr(pms, name)): return getattr(pms, name)
