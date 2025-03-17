a8cedd100bb0937db63954212fb20533fbbabf84
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-17 23:06:13 +0100

    bench: added initial version of bench for pex

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='spk',
     version='0.0.1',
     entry_points = {
-        'console_scripts': ['spk=sparkle.src.core.main:main']
+        'console_scripts': ['spk=sparkle.src.core.main:main',
+                            'bench_pex=sparkle.src.bench.pex:main']
     }
 )
