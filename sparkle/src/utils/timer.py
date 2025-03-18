26f7633cc2986cdd99663b532937a68733b9f802
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-18 13:23:33 +0100

    timer: added @property decorator to dt()

--- sparkle/src/utils/timer.py
+++ sparkle/src/utils/timer.py
@@ -2,7 +2,7 @@
 import time
 
 # Custom imports
-from sparkle.src.utils.prints import spacer
+from sparkle.src.utils.prints import spacer, fmt_float
 
 ###############################################
 ### timer class
@@ -15,18 +15,21 @@ class timer():
     def reset(self):
         self.time_tic = None
         self.time_toc = None
-        self.dt       = 0.0
+        self.dt_      = 0.0
+
+    @property
+    def dt(self):
+        return self.dt_
 
     def tic(self):
         self.t_tic = time.perf_counter()
 
     def toc(self, show=False):
         self.t_toc = time.perf_counter()
-        self.dt   += self.t_toc - self.t_tic
+        self.dt_  += self.t_toc - self.t_tic
 
         if (show): self.show()
 
     def show(self):
-        str_dt = str(f"{self.dt:.2f}")
-        spacer("Timer "+str(self.name)+": "+str_dt+" seconds")
+        spacer("Timer "+str(self.name)+": "+fmt_float(self.dt)+" seconds")
         self.reset()
