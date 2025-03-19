3df6d4fc3c420d2a2c1d96154bcc2babdb7ca0da
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-19 11:02:50 +0100

    pex: added random furthest point sampling

--- sparkle/src/pex/random.py
+++ sparkle/src/pex/random.py
@@ -3,7 +3,9 @@ import math
 import numpy as np
 
 # Custom imports
-from sparkle.src.pex.base import base_pex
+from sparkle.src.pex.base      import base_pex
+from sparkle.src.pex.fps       import fps
+from sparkle.src.utils.default import set_default
 
 ###############################################
 ### Random experiment plan
@@ -18,8 +20,27 @@ class random(base_pex):
     # Reset sampling
     def reset(self):
 
-        # Generate x points for pex
         self.x_ = np.random.uniform(low  = self.xmin,
                                     high = self.xmax,
-                                    size = (self.n_points_,self.dim))
+                                    size = (self.n_points_, self.dim))
 
+###############################################
+### Random experiment plan with fps step
+class random_fps(base_pex):
+    def __init__(self, spaces, pms):
+        super().__init__(spaces, pms)
+
+        self.name     = "random_fps"
+        self.factor   = set_default("factor", 10, pms)
+        self.n_target = self.factor*self.n_points_
+
+        self.reset()
+
+    # Reset sampling
+    def reset(self):
+
+        x = np.random.uniform(low  = self.xmin,
+                              high = self.xmax,
+                              size = (self.n_target, self.dim))
+
+        self.x_ = fps(x, self.n_points_)
