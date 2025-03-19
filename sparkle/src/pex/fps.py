cbc38177f12f79b64119fa2032df4fad00bf3991
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-19 08:29:04 +0100

    fps: now takes np.array as input, and works on indices lists

--- sparkle/src/pex/fps.py
+++ sparkle/src/pex/fps.py
@@ -7,30 +7,33 @@ from sparkle.src.utils.error     import error
 
 ###############################################
 ### Furthest point sampling
-### x: list of point coordinates
+### x: array of point coordinates of shape (n_points, dim)
 ### n_points: target number of points
 def fps(x, n_points):
 
-    if (len(x) < n_points):
+    if (x.shape[0] < n_points):
         error("fps", "fps",
               "Input list has less than required number of points")
 
-    if (len(x) == n_points):
+    if (x.shape[0] == n_points):
         return np.array(x)
 
-    k = np.random.randint(0, len(x))
-    selected = [x[k]]
-    x.pop(k)
+    # Work on indices list for better efficiency
+    lst      = np.arange(x.shape[0]).tolist()
+    k        = np.random.randint(0, x.shape[0])
+    selected = [k]
+    lst.pop(k)
 
     while (len(selected) < n_points):
-        distances = np.zeros((len(x), len(selected)))
+        distances = np.zeros((len(lst), len(selected)))
         for j in range(len(selected)):
-            distances[:,j] = np.linalg.norm(x - selected[j], axis=1)
+            distances[:,j] = np.linalg.norm(x[lst] - x[selected[j]], axis=1)
 
         min_dists = np.min(distances, axis=1)
         k         = np.argmax(min_dists)
 
-        selected.append(x[k])
-        x.pop(k)
+        selected.append(lst[k])
+        lst.pop(k)
 
-    return np.array(selected)
+
+    return x[selected]
