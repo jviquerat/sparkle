6c034069466c4deb7a6b48f10491c8f9b3d2d60c
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-19 08:44:08 +0100

    fps: improved test

--- sparkle/tst/pex/test_fps.py
+++ sparkle/tst/pex/test_fps.py
@@ -1,8 +1,11 @@
 # Generic imports
+import types
 import numpy as np
 
 # Custom imports
 from sparkle.src.pex.fps     import fps
+from sparkle.src.pex.random  import random
+from sparkle.src.env.spaces  import env_spaces
 from sparkle.src.utils.seeds import set_seeds
 
 ###############################################
@@ -11,17 +14,28 @@ def test_fps():
 
     set_seeds(0)
 
-    x = np.array([[0.0, 0.0],
-                  [0.1, 0.7],
-                  [0.4, 0.2],
-                  [0.4, 0.9],
-                  [0.8, 0.1],
-                  [0.2, 0.2]])
+    dim      = 2
+    xmin     = np.array([0.0, 0.0])
+    xmax     = np.array([1.0, 1.0])
+    n_points = 100
 
-    x_fps = fps(x, 4)
-    ref = np.array([[0.8, 0.1],
-                    [0.1, 0.7],
-                    [0.0, 0.0],
-                    [0.4, 0.2]])
+    pms          = types.SimpleNamespace()
+    pms.n_points = n_points
 
-    assert(np.allclose(x_fps, ref))
+    loc_space = {"dim": dim, "x0": None, "xmin": xmin, "xmax": xmax}
+    s = env_spaces(loc_space)
+    pex = random(s, pms)
+
+    x   = fps(pex.x, 10)
+    ref = np.array([[0.8965466 , 0.36756187],
+                    [0.0202184 , 0.83261985],
+                    [0.19999652, 0.01852179],
+                    [0.7142413 , 0.99884701],
+                    [0.45615033, 0.56843395],
+                    [0.05802916, 0.43441663],
+                    [0.7044144 , 0.03183893],
+                    [0.34535168, 0.92808129],
+                    [0.97861834, 0.79915856],
+                    [0.46631077, 0.24442559]])
+
+    assert(np.allclose(x, ref))
