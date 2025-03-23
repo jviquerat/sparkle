9a89477461ae15a45e331e74048d883bd791764a
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-03-23 19:44:45 +0100

    lint: fixed comparison style in tst::compare

--- sparkle/tst/utils/test_compare.py
+++ sparkle/tst/utils/test_compare.py
@@ -14,26 +14,22 @@ def test_compare():
     r1 = 1.0000000001
     r2 = 1.0
     re = 1.0e-9
-    smaller = compare(r1,r2,re)
-    assert(smaller == True)
+    assert compare(r1,r2,re)
 
     # Comparing false
     r1 = 1.0000000001
     r2 = 1.0
     re = 1.0e-11
-    smaller = compare(r1,r2,re)
-    assert(smaller == False)
+    assert not compare(r1,r2,re)
 
     # Case that compares to true for weak and to false for strong
     r1 = 1.0e-10
     r2 = 1.1e-10
     re = 0.099
-    smaller = compare(r1,r2,re)
-    assert(smaller == False)
+    assert not compare(r1,r2,re)
 
     # Test below min
     r1 = 0.1*sys.float_info.min
     r2 = 1.0
     re = sys.float_info.min
-    smaller = compare(r1,r2,re)
-    assert(smaller == True)
+    assert compare(r1,r2,re)
