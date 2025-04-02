4c86e4f5d514dbbe06f98111f194611c3d0fc4b5
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-02 13:21:11 +0200

    lint: typing kernel/

--- sparkle/src/kernel/gaussian.py
+++ sparkle/src/kernel/gaussian.py
@@ -1,21 +1,30 @@
 # Generic imports
 import numpy as np
+from types import SimpleNamespace
+from typing import Optional
 
 # Custom imports
 from sparkle.src.kernel.base     import BaseKernel
 from sparkle.src.utils.distances import distance_all_to_all
+from numpy import ndarray
+from sparkle.src.env.spaces import EnvSpaces
 
 ###############################################
 ### Isotropic gaussian kernel
 class Gaussian(BaseKernel):
-    def __init__(self, spaces, pms=None):
+    def __init__(self,
+                 spaces: EnvSpaces,
+                 pms: Optional[SimpleNamespace]=None) -> None:
         super().__init__(spaces)
 
         self.dim_ = 1
 
     # Compute isotropic covariance function
     # xi and xj have shapes (n_batch, dim)
-    def covariance(self, xi, xj, theta):
+    def covariance(self,
+                   xi: ndarray,
+                   xj: ndarray,
+                   theta: ndarray) -> ndarray:
 
         dist = distance_all_to_all(xi, xj)
         v    = np.exp(-0.5*(dist/theta[0])**2)
--- sparkle/src/kernel/matern52.py
+++ sparkle/src/kernel/matern52.py
@@ -1,22 +1,31 @@
 # Generic imports
 import math
 import numpy as np
+from numpy import ndarray
+from types import SimpleNamespace
+from typing import Optional
 
 # Custom imports
 from sparkle.src.kernel.base     import BaseKernel
 from sparkle.src.utils.distances import distance_all_to_all
+from sparkle.src.env.spaces import EnvSpaces
 
 ###############################################
 ### Isotropic Matern 5/2 kernel
 class Matern52(BaseKernel):
-    def __init__(self, spaces, pms=None):
+    def __init__(self,
+                 spaces: EnvSpaces,
+                 pms: Optional[SimpleNamespace]=None) -> None:
         super().__init__(spaces)
 
         self.dim_ = 1
 
     # Compute isotropic covariance function
     # xi and xj have shapes (n_batch, dim)
-    def covariance(self, xi, xj, theta):
+    def covariance(self,
+                   xi: ndarray,
+                   xj: ndarray,
+                   theta: ndarray) -> ndarray:
 
         dist = distance_all_to_all(xi, xj)
         v0   = (1.0 + math.sqrt(5.0)*dist/theta[0] + 5.0/(3.0*theta[0])*dist**2)
