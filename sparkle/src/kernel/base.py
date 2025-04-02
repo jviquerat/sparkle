8b9b5df9778ebdfde82bbf160567e8e6763444e5
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-02 13:21:11 +0200

    lint: typing kernel/

--- sparkle/src/kernel/base.py
+++ sparkle/src/kernel/base.py
@@ -1,21 +1,23 @@
 # Generic imports
 import numpy as np
-from   numpy import matmul
+from   numpy import ndarray, matmul
 from   numpy.linalg import solve
+from typing import Optional
 
 # Custom imports
 from sparkle.src.agent.ms_lbfgsb import MSLBFGSB
 from sparkle.src.utils.distances import min_max_distance
+from sparkle.src.env.spaces import EnvSpaces
 
 ###############################################
 ### Base kernel
 class BaseKernel():
-    def __init__(self, spaces):
+    def __init__(self, spaces: EnvSpaces) -> None:
 
         self.spaces = spaces
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
 
         self.x0_       = None
         self.xmin_     = None
@@ -24,10 +26,10 @@ class BaseKernel():
         self.diag_eps_ = 1.0e-15
 
     @property
-    def theta(self):
+    def theta(self) -> None:
         return self.theta_
 
-    def optimize(self, x, y):
+    def optimize(self, x: ndarray, y: ndarray) -> None:
 
         # Copy (x,y)
         self.x_  = x
@@ -50,7 +52,7 @@ class BaseKernel():
         self.theta_ = np.exp(x_opt)
 
     # Compute log-likelihood
-    def log_likelihood(self, log_theta):
+    def log_likelihood(self, log_theta: ndarray) -> float:
 
         theta = np.exp(log_theta)
 
@@ -68,7 +70,10 @@ class BaseKernel():
 
     # () operator
     # x and y have shapes (n_batch, dim)
-    def __call__(self, xi, xj, theta=None):
+    def __call__(self,
+                 xi: ndarray,
+                 xj: ndarray,
+                 theta: Optional[ndarray]=None) -> ndarray:
 
         if theta is None: theta = self.theta_
 
