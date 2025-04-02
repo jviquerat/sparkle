68bb861fc1d6d55523dbe7bddb1dcae0d30fbc9b
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-02 16:15:54 +0200

    docstrings: infill/

--- sparkle/src/infill/ei.py
+++ sparkle/src/infill/ei.py
@@ -11,18 +11,48 @@ from sparkle.src.model.kriging import Kriging
 ###############################################
 ### Expected improvement infill
 class EI():
+    """
+    Expected Improvement (EI) infill criterion.
+
+    This class implements the Expected Improvement infill criterion, which
+    is used in Bayesian optimization to select the next point to evaluate.
+    It balances exploration and exploitation by choosing points that are
+    likely to improve upon the current best observation.
+    """
     def __init__(self, spaces: EnvSpaces, model: Any) -> None:
+        """
+        Initializes the EI infill criterion.
+
+        Args:
+            spaces: The environment's search space definition.
+            model: The surrogate model (e.g., Kriging) used for function approximation.
+        """
 
         self.spaces = spaces
         self.model  = model
 
     def set_best(self, xb: ndarray, yb: float) -> None:
+        """
+        Sets the current best observation.
+
+        Args:
+            xb: The best point found so far.
+            yb: The function value at the best point.
+        """
 
         self.xb = xb
         self.yb = yb
 
-    # Actual infill criterion
     def _ei(self, x: ndarray) -> ndarray:
+        """
+        Computes the Expected Improvement at a set of points.
+
+        Args:
+            x: A NumPy array of points at which to compute the EI.
+
+        Returns:
+            A NumPy array of the EI values at the given points.
+        """
 
         x       = np.reshape(x, (-1,self.spaces.dim))
         mu, std = self.model.evaluate(x)
@@ -38,7 +68,18 @@ class EI():
 
         return ei
 
-    # () operator used for optimization
     def __call__(self, x: ndarray) -> ndarray:
+        """
+        Computes the Expected Improvement at a set of points.
+
+        This method is an alias for _ei, allowing the EI object to be called
+        as a function.
+
+        Args:
+            x: A NumPy array of points at which to compute the EI.
+
+        Returns:
+            A NumPy array of the EI values at the given points.
+        """
 
         return self._ei(x)
--- sparkle/src/infill/log_ei.py
+++ sparkle/src/infill/log_ei.py
@@ -9,20 +9,50 @@ from sparkle.src.model.kriging import Kriging
 
 
 ###############################################
-### Log expected improvement infill
 class LogEI():
+    """
+    Log Expected Improvement (LogEI) infill criterion.
+
+    This class implements the Log Expected Improvement infill criterion, a
+    variant of the Expected Improvement that uses a logarithmic transformation
+    to improve numerical stability. It is used in Bayesian optimization to
+    select the next point to evaluate.
+    """
     def __init__(self, spaces: EnvSpaces, model: Any) -> None:
+        """
+        Initializes the LogEI infill criterion.
+
+        Args:
+            spaces: The environment's search space definition.
+            model: The surrogate model (e.g., Kriging) used for function approximation.
+        """
 
         self.spaces = spaces
         self.model  = model
         self.bound  =-1.0e8
 
     def set_best(self, xb: ndarray, yb: float) -> None:
+        """
+        Sets the current best observation.
+
+        Args:
+            xb: The best point found so far.
+            yb: The function value at the best point.
+        """
 
         self.xb = xb
         self.yb = yb
 
     def _log_ei(self, x: ndarray) -> ndarray:
+        """
+        Computes the Log Expected Improvement at a set of points.
+
+        Args:
+            x: A NumPy array of points at which to compute the LogEI.
+
+        Returns:
+            A NumPy array of the LogEI values at the given points.
+        """
 
         x       = np.reshape(x, (-1,self.spaces.dim))
         mu, std = self.model.evaluate(x)
@@ -49,23 +79,52 @@ class LogEI():
 
         return lgei
 
-    # () operator used for optimization
     def __call__(self, x: ndarray) -> ndarray:
+        """
+        Computes the Log Expected Improvement at a set of points.
+
+        This method is an alias for _log_ei, allowing the LogEI object to be
+        called as a function.
+
+        Args:
+            x: A NumPy array of points at which to compute the LogEI.
+
+        Returns:
+            A NumPy array of the LogEI values at the given points.
+        """
 
         return self._log_ei(x)
 
-# Numerically stable version of log(1 - exp(x))
 def log1mexp(x: float) -> float:
+    """
+    Numerically stable version of log(1 - exp(x)).
+
+    Args:
+        x: The input value.
+
+    Returns:
+        The result of log(1 - exp(x)).
+    """
 
     if (x > -log(2)):
         return log(-expm1(x))
     else:
         return log1p(-exp(x))
 
-# erfcx approximation from:
-# "Closed-form approximations to the error and complementary error
-#  functions and their applications in atmospheric science", Ren et al (2007)
 def erfcx(x: float) -> float:
+    """
+    Approximation of the scaled complementary error function.
+
+    This function uses an approximation from:
+    "Closed-form approximations to the error and complementary error
+    functions and their applications in atmospheric science", Ren et al (2007)
+
+    Args:
+        x: The input value.
+
+    Returns:
+        The approximate value of erfcx(x).
+    """
 
     a = 2.9110
     v = a/((a-1.0)*sqrt(pi*x*x) + sqrt(pi*x*x + a*a))
