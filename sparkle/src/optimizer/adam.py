a5620b7d3df98e340a127046a44ca46a96974982
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-02 12:58:57 +0200

    lint: typing optimizer/

--- sparkle/src/optimizer/adam.py
+++ sparkle/src/optimizer/adam.py
@@ -1,19 +1,22 @@
 # Generic imports
 import torch.optim as toptim
+from types import SimpleNamespace
+
+from sparkle.src.network.mlp import MLP
 
 ###############################################
 ### Adam optimizer class
 class Adam():
-    def __init__(self, model, pms):
+    def __init__(self, model: MLP, pms: SimpleNamespace) -> None:
 
         self.model_ = model
         self.opt_   = toptim.Adam(self.model_.params(),
                                   lr=pms.lr)
 
-    def zero_grad(self):
+    def zero_grad(self) -> None:
 
         self.opt_.zero_grad()
 
-    def step(self):
+    def step(self) -> None:
 
         self.opt_.step()
