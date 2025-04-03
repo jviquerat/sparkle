2c15016dcbf0ee3b2026b2e04306b01b488f517d
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-03 09:07:59 +0200

    ego: removed test from loaded model

--- sparkle/tst/agent/test_ego.py
+++ sparkle/tst/agent/test_ego.py
@@ -13,14 +13,3 @@ def test_ego():
     # Run test
     runner("sparkle/tst/agent/json/parabola_ego.json",
            1.286324780000000e+01, 9.951025999999998e-01)
-
-###############################################
-### Test ego with loaded model
-def test_ego_load_model():
-
-    # Add environment to PATH
-    sys.path.append("sparkle/env/sinebump")
-
-    # Run test
-    runner("sparkle/tst/agent/json/sinebump_ego_load.json",
-           2.754777996000001e+00, -1.756220000000000e+00)
