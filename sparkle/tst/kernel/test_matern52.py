23a1faf39a97bc069151bcfa1bf0d224062eab0d
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-08 14:58:26 +0200

    kernel: added derivatives for gaussian and matern52 with tests

--- sparkle/tst/kernel/test_matern52.py
+++ sparkle/tst/kernel/test_matern52.py
@@ -6,7 +6,6 @@ from sparkle.src.utils.seeds import set_seeds
 
 
 ###############################################
-### Test matern52 kernel
 def test_matern52():
 
     # Set seed for reproducible test
@@ -18,22 +17,57 @@ def test_matern52():
                   "xmax": np.array([1,1])}
     space      = EnvSpaces(space_dict)
 
+    # Check base kernel
     kernel = Matern52(space)
     x0     = np.array([[0.5,0.5]])
     x1     = np.array([[0.6,0.6]])
-    theta  = np.array([0.1])
+    theta  = np.array([0.1, 0.1])
 
     K = kernel(x0, x1, theta)
-    K_ref = np.array([[0.1902957050844289]])
+    K_ref = np.array([[0.00317283]])
     assert(np.allclose(K, K_ref))
 
+    # Check shapes
     x0     = np.array([[0.5,0.5],
                        [0.4,0.4]])
     x1     = np.array([[0.6,0.6],
                        [0.1,0.1]])
-    theta  = np.array([0.1])
+    theta  = np.array([0.1, 0.1])
 
     K = kernel(x0, x1, theta)
-    K_ref = np.array([[1.90295705e-01, 6.09415049e-05],
-                      [1.55128831e-02, 1.02289432e-03]])
+    K_ref = np.array([[3.17283364e-03, 2.15041379e-06],
+                      [3.70140371e-04, 3.07068022e-05]])
     assert(np.allclose(K, K_ref))
+
+    # Check derivative wrt x
+    kernel = Matern52(space)
+    x0     = np.array([[0.5,0.5]])
+    x1     = np.array([[0.6,0.6]])
+    theta  = np.array([0.1, 0.1])
+
+    K = kernel(x0, x1, theta)
+    dKdx = kernel.covariance_dx(x0, x1, theta)
+
+    eps = 1.0e-8
+    dx = np.array([[eps, 0.0]])
+    dy = np.array([[0.0, eps]])
+    dKdx_fd = np.array([kernel(x0+dx, x1, theta) - kernel(x0-dx, x1, theta),
+                        kernel(x0+dy, x1, theta) - kernel(x0-dy, x1, theta)])/(2.0*eps)
+    dKdx_fd = dKdx_fd.reshape(dKdx.shape)
+
+    assert np.allclose(dKdx, dKdx_fd)
+
+    # Check derivative wrt theta
+    dKdt = kernel.covariance_dtheta(x0, x1, theta)
+
+    eps = 1.0e-8
+    dx = np.array([eps, 0.0])
+    dy = np.array([0.0, eps])
+    dKdt_fd = np.array([kernel(x0, x1, theta+dx) - kernel(x0, x1, theta-dx),
+                        kernel(x0, x1, theta+dy) - kernel(x0, x1, theta-dy)])/(2.0*eps)
+    dKdt_fd = dKdt_fd.reshape(dKdt.shape)
+
+    print(dKdt)
+    print(dKdt_fd)
+
+    assert np.allclose(dKdt, dKdt_fd)
