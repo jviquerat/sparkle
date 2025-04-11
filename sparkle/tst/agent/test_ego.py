0fdaf6d27e2c93e613b451011c8214d3b30e4bb4
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-11 13:12:27 +0200

    tst: cleaning files after tests

--- sparkle/tst/agent/test_ego.py
+++ sparkle/tst/agent/test_ego.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from sparkle.tst.runner import runner
@@ -12,3 +13,6 @@ def test_ego():
     # Run test
     runner("sparkle/tst/agent/json/parabola_ego.json",
            1.790231330000000e+00, 6.684993000000000e-02)
+
+    filename = "kriging.dat"
+    os.remove(filename)
