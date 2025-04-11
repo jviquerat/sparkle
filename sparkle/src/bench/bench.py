e0b5511d8c5a441b20089687e9535cc1834217d1
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-11 12:59:42 +0200

    bench: moved parameter combination to bench/bench.py

---
+++ sparkle/src/bench/bench.py
@@ -0,0 +1,37 @@
+import itertools
+from typing import Any, List
+from collections import defaultdict
+
+from sparkle.src.utils.prints import spacer
+
+
+def combine_parameters(keys: List[str], values: List[Any]) -> List[dict]:
+    """
+    A function to combine parameters based on a list of keys and a list of values
+
+    Args:
+        keys: list of keys
+        values: list of values
+
+    Returns:
+        combinations: a list of dicts, each containing a combination of
+                      the different values
+
+    """
+    # Printings
+    spacer("Parameter keys: "+str(keys))
+    spacer("Parameter values: "+str(values))
+
+    # Generate combinations as list of tuples
+    comb_tuples = list(itertools.product(*values))
+
+    # Convert list of tuples to list of dicts
+    combinations = []
+    for k in comb_tuples:
+        comb_dict = defaultdict(list)
+        for l in range(len(k)):
+            comb_dict[keys[l]] = k[l]
+        combinations.append(dict(comb_dict))
+    spacer("Nb of combinations: "+str(len(combinations)))
+
+    return combinations
---
+++ sparkle/tst/bench/test_bench.py
@@ -0,0 +1,22 @@
+import types
+
+import pytest
+import numpy as np
+
+from sparkle.src.utils.json import JsonParser
+from sparkle.src.bench.bench import combine_parameters
+
+###############################################
+def test_bench():
+
+    parser    = JsonParser()
+    filename = "sparkle/tst/bench/pex.json"
+    pms       = parser.read(filename)
+    methods    = pms.methods
+    dimensions = pms.dimensions
+
+    keys   = ["method", "dimension"]
+    values = [ methods,  dimensions]
+
+    combinations = combine_parameters(keys, values)
+    assert len(combinations) == 12
