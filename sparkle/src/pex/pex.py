d41fc0c344e0df6bbd7d1f41bf88bf98c7f5e72c
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-14 11:23:18 +0200

    MLHS: added a SA-based version for acceptance scheme

--- sparkle/src/pex/pex.py
+++ sparkle/src/pex/pex.py
@@ -2,6 +2,7 @@ from sparkle.src.core.factory import Factory
 from sparkle.src.pex.fpd import FPD
 from sparkle.src.pex.lhs import LHS
 from sparkle.src.pex.mlhs import MLHS
+from sparkle.src.pex.mlhs_sa import MLHS_SA
 from sparkle.src.pex.random import Random, RandomFPS
 
 # Declare factory
@@ -11,5 +12,6 @@ pex_factory = Factory()
 pex_factory.register("random",     Random)
 pex_factory.register("lhs",        LHS)
 pex_factory.register("mlhs",       MLHS)
+pex_factory.register("mlhs_sa",    MLHS_SA)
 pex_factory.register("fpd",        FPD)
 pex_factory.register("random_fps", RandomFPS)
