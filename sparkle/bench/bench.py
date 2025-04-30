324191e5e44c81856d2b4281e68ed181dbb4a3cb
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-04-30 17:41:11 +0200

    bench: added lbfgsb bench

--- sparkle/bench/bench.py
+++ sparkle/bench/bench.py
@@ -1,9 +1,11 @@
 from sparkle.src.core.factory import Factory
 from sparkle.bench.pex import BenchPex
+from sparkle.bench.lbfgsb import BenchLBFGSB
 
 # Declare factory
 bench_factory = Factory()
 
 # Register benchmarks
 bench_factory.register("pex", BenchPex)
+bench_factory.register("lbfgsb", BenchLBFGSB)
 
