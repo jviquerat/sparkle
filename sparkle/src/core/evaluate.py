2258a0e3988078ed60e970cf26453f4cce1e7ddd
Jonathan <jonathan.viquerat@mines-paristech.fr>
2025-05-07 11:18:02 +0200

    evaluate: added screen output of best element

--- sparkle/src/core/evaluate.py
+++ sparkle/src/core/evaluate.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from sparkle.src.env.parallel import parallel
 from sparkle.src.utils.json import JsonParser
+from sparkle.src.utils.prints import spacer, fmt_float
 
 
 def evaluate(dat_file, json_file):
@@ -41,10 +42,14 @@ def evaluate(dat_file, json_file):
     s = data[-1,3] # best step
     x = data[int(s),4:] # best x
 
+    assert c == data[int(s),2]
+
     # Run environment
     x = np.array([x])
     c = env.cost(x)
     env.render(x, c)
+
+    spacer(f"Best cost c={fmt_float(c[0])} at x={x[0]}")
 
     # Finalize
     env.close()
